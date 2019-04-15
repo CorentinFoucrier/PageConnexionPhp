98d683db5e34919c866f8c8006905df2eddf542d
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-04-15 15:31:24 +0200

    remplisage page profile

--- page.php
+++ page.php
@@ -3,16 +3,19 @@
 	if (isset($_GET['deconnect']) && $_GET['deconnect']) {
 		session_unset();
 		header("Location: http://github.local/PageConnexionPhp/index.php");
+		exit;
 	}
 	$connect = $_SESSION['username'];
 	if (empty($_SESSION)) {
+		header("HTTP/1.0 403 Forbidden");
 		header("Location: http://github.local/PageConnexionPhp/index.php");
+		exit;
 	}
 	$username = $_SESSION['username'];
 	
 	echo "super site <br />";
 	echo "Bonjour {$username} !"
-?>*
-
+?>
+<br />
 <a href="http://github.local/PageConnexionPhp/profile.php"><button>Profil</button></a>
 <a href="http://github.local/PageConnexionPhp/page.php?deconnect=true"><button>Deconnexion</button></a>
--- profile.php
+++ profile.php
@@ -1,11 +1,21 @@
 <?php
 
 	session_start();
+	if (isset($_GET['deconnect']) && $_GET['deconnect']) {
+		session_unset();
+		header("Location: http://github.local/PageConnexionPhp/index.php");
+		exit;
+	}
 	if (!isset($_SESSION['connect'])) {
+		header("HTTP/1.0 403 Forbidden");
 		header("Location: http://github.local/PageConnexionPhp/page.php");
+		exit;
 	}
 	$username = $_SESSION['username'];
 
-	echo "My name is {$username}";
+	echo "My name is ".substr($username, 3).", ".substr($username, 0, 3)."-".substr($username, 3);
 
 ?>
+
+<br />
+<a href="http://github.local/PageConnexionPhp/profile.php?deconnect=true"><button>Deconnexion</button></a>
